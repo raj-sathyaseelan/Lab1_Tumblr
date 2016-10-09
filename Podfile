99c90a311f937d05dce2c0180476b30813c59a25
raj-sathyaseelan <RajSathya@ac02qm2rkg8wm.attlocal.net>
2016-10-08 22:19:08 -0700

    added cocoa pods

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'tumblr' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for tumblr
+
+  target 'tumblrTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'tumblrUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+pod 'AFNetworking', '~> 2.6'
+
+end
--- tumblr/ViewController.swift
+++ tumblr/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    @IBOutlet weak var movieTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        movieTableView.rowHeight = 200.0
+        movieTableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //let cell = UITableViewCell(style: .default, reuseIdentifier: "")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableCell") as! TableCell
+        cell.mycustomLabel?.text = "Row \(indexPath.row)"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController(nibName: nil, bundle: nil)
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
 
 }
 
+class TableCell: UITableViewCell {
+    
+    @IBOutlet weak var mycustomLabel: UILabel!
+    
+}
+
+class DetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let dv = UIView(frame: CGRect.zero)
+        dv.backgroundColor = UIColor.brown
+        self.view = dv
+        
+    }
+}
